c9ed7994dc8c68aceba7c7ba0fbb3bdfaba8054c
trevor-lowder <lowder.trevor@yahoo.com>
2024-04-23 10:34:29 -0700

    finish 1.5

--- exercises/part1/courseinfo/src/App.jsx
+++ exercises/part1/courseinfo/src/App.jsx
@@ -1,54 +1,49 @@
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map((part) => (
+      <p key={part.name}>
+        {part.name} - {part.exercises}
+      </p>
+    ))}
+  </>
+);
+
+const Total = ({ total }) => {
+  return <p>Number of exercises: {total}</p>;
+};
+
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-  const total = exercises1 + exercises2 + exercises3;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7
+      },
+      {
+        name: "State of a component",
+        exercises: 14
+      }
+    ]
+  };
 
   return (
     <>
-      <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total
+        total={course.parts.reduce((acc, part) => acc + part.exercises, 0)}
       />
-      <Total total={total} />
     </>
   );
 };
 
 export default App;
-
-const Header = (course) => {
-  return <h1>{course}</h1>;
-};
-
-const Content = (part1, part2, part3, exercises1, exercises2, exercises3) => {
-  return (
-    <>
-      <p>
-        {part1}
-        {exercises1}
-      </p>
-      <p>
-        {part2}
-        {exercises2}
-      </p>
-      <p>
-        {part3}
-        {exercises3}
-      </p>
-    </>
-  );
-};
-
-const Total = (total) => {
-  return <p>Number of exercises: {total}</p>;
-};
