312002bf96e00e8893f67711ab09bf2e9fb4716c
trevor-lowder <lowder.trevor@yahoo.com>
2024-04-23 08:47:03 -0700

    finish step 1.2

--- exercises/part1/courseinfo/src/App.jsx
+++ exercises/part1/courseinfo/src/App.jsx
@@ -1,8 +1,54 @@
-
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+  const total = exercises1 + exercises2 + exercises3;
 
-export default App
+  return (
+    <>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total total={total} />
+    </>
+  );
+};
+
+export default App;
+
+const Header = (course) => {
+  return <h1>{course}</h1>;
+};
+
+const Content = (part1, part2, part3, exercises1, exercises2, exercises3) => {
+  return (
+    <>
+      <p>
+        {part1}
+        {exercises1}
+      </p>
+      <p>
+        {part2}
+        {exercises2}
+      </p>
+      <p>
+        {part3}
+        {exercises3}
+      </p>
+    </>
+  );
+};
+
+const Total = (total) => {
+  return <p>Number of exercises: {total}</p>;
+};
